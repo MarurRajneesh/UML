e27cb9db86e9b19840ca29bb9f9002c9db910835
Rajneesh69 <rajneesh.theraj@gmail.com>
2017-01-31 18:18:10 +0530

    UML-First Draft

---
+++ Programs/SlideDeck/Slide.java
@@ -0,0 +1,45 @@
+public class Slide
+{
+	private String title;
+	private String content;
+
+	public Slide()
+	{
+
+	}
+
+	public Slide(SlideDeck deck)
+	{
+		assert(deck != null);
+	}
+
+	public void setTitle(String title)
+	{
+		assert(title!=null);
+		title=title.trim();
+		assert(title.length()>0);
+		this.title=title;
+	}
+
+	public String getTitle(){return this.title;}
+
+	public void setContent(String text)
+	{
+		if(text==null || text.trim().length()==0)
+		{
+			this.content="";
+		}
+		else
+		{
+			this.content=text.trim();
+		}
+	}
+
+	public String getContent(){return this.content;}
+
+	protected void reset()
+	{
+		this.title=title;
+		this.content=content;
+	}
+}
